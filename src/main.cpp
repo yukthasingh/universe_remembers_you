91ae12c242eabb8ebe2daf12f3050d24b01adbb7
yukthasingh <yuktha.aruna.singh@outlook.com>
2026-09-03 17:52:37 -0400

    Added necessary files, made starter code for main

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+// #include <SFML/graphics.hpp>
+// #include "universe.h"
+// #include "renderer.h"
+
+int main() {
+    std::cout << "Hello";
+
+    return 0;
+}
